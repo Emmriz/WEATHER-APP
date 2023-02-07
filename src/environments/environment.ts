9a313f12e8bb8c2478cffce2bf5eef03638ca2f0
Ajolore Emmanuel Babatunde <emmanuelajolore@gmail.com>
2023-02-07 17:52:41 +0100

    Added weather service and created a model for the Json weather file

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+
+    weatherApiBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host', 
+    XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    // XRapidAPIKeyHeaderValue: '68c6c515bamshc5a594f682d6038p11b167jsn01f71a03bff1'
+    XRapidAPIKeyHeaderValue: '68c6c515bamshc5a594f682d6038p11b167jsn01f71a03bff1'
+};
